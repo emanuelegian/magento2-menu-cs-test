40d6c7e8a4f0e7cce9f8fa12e064cc06cdd0c43f
Emanuele Gian <emanuelegian@gmail.com>
2021-05-13 15:07:59 +0200

    test CS

---
+++ Block/Menu/TestCs.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EmanueleGian\Block;
+
+class TestCs
+{
+    public function MyUglyMethod()
+    {
+        return 'This is a looong phrase. This is a looong phrase. This is a looong phrase. This is a looong phrase. This is a looong phrase. This is a looong phrase. This is a looong phrase. ';
+    }
+
+    public function MyUglyMethodAgain()
+    {
+        return 'This is a looong phrase. This is a looong phrase. This is a looong phrase. This is a looong phrase. This is a looong phrase. This is a looong phrase. This is a looong phrase. ';
+    }
+
+
+    public function MyUglyMethodAgainAgain()
+    {
+        return 'This is a looong phrase. This is a looong phrase. This is a looong phrase. This is a looong phrase. This is a looong phrase. This is a looong phrase. This is a looong phrase. ';
+    }
+
+
+}
+
+
+
